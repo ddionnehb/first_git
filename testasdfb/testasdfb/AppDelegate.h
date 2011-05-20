a49fb990f6e436db4d9a934251f76f83bd2b8e1b
Dennis Dionne <ddionnehb@yahoo.com>
2011-05-20 00:54:00 -0700

    asfasdfff

---
+++ testasdfb/testasdfb/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  testasdfb
+//
+//  Created by Dennis Dionne on 5/20/11.
+//  Copyright Self 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ testasdfb/testasdfb/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  testasdfb
+//
+//  Created by Dennis Dionne on 5/20/11.
+//  Copyright Self 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
